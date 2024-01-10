459ff19b975dd1460b15167e7eeef89baa748cc7
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-10 03:48:42 +0100

    💻Updated App

--- app.py
+++ app.py
@@ -5,7 +5,6 @@
 from modules import *  # Import all custom modules in /modules dir
 
 import tkinter as tk
-import ttkbootstrap as ttk
 import customtkinter as ctk
 
 # ----------------------------------- Setup ---------------------------------- #
@@ -15,16 +14,39 @@ app.title("PassGen")
 app.geometry("800x500")
 app.resizable(False, False)
 
-# Load custom ttkb themes
-ttk.Style().load_user_themes(file="assets/themes/ttkb-themes.json")  
-
 
 # ---------------------------------- Widgets --------------------------------- #
+# Main frames
+frame_password = ctk.CTkFrame(app, corner_radius=10)
+frame_evaluator = ctk.CTkFrame(app, corner_radius=10)
+frame_customize = ctk.CTkFrame(app, corner_radius=10)
 
+# Frame password
+label_password_var = tk.StringVar(value=PasswordGenerator(length=15).generate_password())
+label_password = ctk.CTkLabel(frame_password, textvariable=label_password_var, font=("Arial Bold", 25))
+
+
+# ---------------------------------- Layout ---------------------------------- #
+# Configure app grid
+app.columnconfigure((0,1,2,3,4), weight=1, uniform="a")
+app.rowconfigure((0,1,2,3,4), weight=1, uniform="a")
+
+# Frame password
+label_password.place(relx=0.02, rely=0.5, anchor="w")
+frame_password.grid(column=0, row=0, columnspan=4, sticky="nsew", padx=10, pady=10)
+
+# Frame evaluator
+frame_evaluator.grid(column=4, row=0, sticky="nsew", padx=10, pady=10)
+
+# Frame customize
+frame_customize.grid(column=0, row=1, columnspan=5, rowspan=5, sticky="nsew", padx=10, pady=10)
 
 
 # ------------------------------------ Run ----------------------------------- #
+# Set default theme
 ChangeTheme(app).set_dark_theme()
+
+# Run the app
 app.mainloop()
 
 
--- modules/themes.py
+++ modules/themes.py
@@ -2,7 +2,6 @@
 #                             PassGen Themes Module                            #
 # ---------------------------------------------------------------------------- #
 
-import tkinter as tk
 import ttkbootstrap as ttk
 import customtkinter as ctk
 
@@ -16,10 +15,8 @@ class ChangeTheme:
         self.app.iconbitmap("assets/icon/icon-white.ico")  # Load icon
         ctk.set_appearance_mode("dark")  # Set ctk theme
         ctk.set_default_color_theme("green")  # Set default ctk color
-        ttk.Style().theme_use("darkly-ctk")  # Set the ttkbootstrap theme
 
     def set_light_theme(self):
         self.app.iconbitmap("assets/icon/icon-black.ico")  # Load icon
         ctk.set_appearance_mode("light") # Set ctk theme
         ctk.set_default_color_theme("green")  # Set default ctk color
-        ttk.Style().theme_use("litera-ctk")  # Set the ttkbootstrap theme
